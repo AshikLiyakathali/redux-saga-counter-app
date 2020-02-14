edcb1133985fb0ad299ce0e7726b64f1142b610f
Ashik Liyakathali <ashik21395@gmail.com>
2020-02-14 12:32:23 -0500

    created service calls

--- service/index.js
+++ service/index.js
@@ -1,1 +1,42 @@
-import React from "react";
+export function GetDataFromServer(apiPath, reqMethod, formBody) {
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  if (!reqMethod && reqMethod !== "POST") {
+    return fetch(apiPath, { method: "GET", headers: myHeaders });
+  } else {
+    if (formBody) {
+      let fetchData = {
+        method: "POST",
+        body: JSON.stringify(formBody),
+        headers: myHeaders
+      };
+      return fetch(apiPath, fetchData);
+    }
+  }
+}
+
+export function GetDataFromServerToPost(apiPath, reqMethod, formBody) {
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  if (formBody) {
+    let fetchData = {
+      method: "POST",
+      body: formBody,
+      headers: myHeaders
+    };
+    return fetch(apiPath, fetchData);
+  }
+}
+
+export function deleteTodoAPI(formBody) {
+  console.log("FORM BODY" + JSON.stringify(formBody));
+  let myHeaders = new Headers();
+
+  myHeaders.append("Content-Type", "application/json");
+  return fetch("http://18.222.167.189:5000/delete-project", {
+    method: "DELETE",
+    body: JSON.stringify(formBody),
+    headers: myHeaders
+  }).then(res => res.json());
+}
